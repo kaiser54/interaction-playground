d4e1500adf7d485c61d8624d6d99a874feda8a84
Temitope Agboola <agboolatemitope070@gmail.com>
2026-04-26 14:03:45 +0100

    p-4

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="pb-40">
       <RouterProvider router={router} />
-      <section className="container-wrapper space-y-4 w-full">
+      <section className="container-wrapper space-y-4 w-full p-4">
         <h2 className="text-sm font-medium text-default">Get in touch</h2>
         <p className="text-sm text-neutral">I'm always looking for new opportunities and collaborations. Feel free to reach out to me via email or LinkedIn.</p>
         <div className="pt-2 space-y-2 text-sm text-neutral flex items-center gap-4">
